3b7fbb92b0ee2f9a977de24c27510dea60b31bad
Aksel CAKMAK <akselcakmak@gmail.com>
2017-01-12 14:35:54 +0000

    Add files via upload

--- sw5.sql
+++ sw5.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Jan 11, 2017 at 12:53 AM
+-- Generation Time: Jan 12, 2017 at 03:35 PM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.10
 
@@ -29,18 +29,25 @@ SET time_zone = "+00:00";
 CREATE TABLE `snippets` (
   `snippet_id` int(11) NOT NULL,
   `username` text NOT NULL,
-  `snippet` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  `snippet` text NOT NULL,
+  `isPublic` tinyint(1) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `snippets`
 --
 
-INSERT INTO `snippets` (`snippet_id`, `username`, `snippet`) VALUES
-(1, 'userxyz', 'some text'),
-(2, 'userxyz', 'lorem some ipsum'),
-(3, 'user123', 'some other cool nice paragraph'),
-(4, 'user123', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
+INSERT INTO `snippets` (`snippet_id`, `username`, `snippet`, `isPublic`) VALUES
+(1, 'userxyz', 'some text', 0),
+(2, 'userxyz', 'Some public text ! Lorem some ipsum.', 1),
+(3, 'user123', 'Some other nice public stuff ! My cool nice paragraph.', 1),
+(4, 'user123', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 0),
+(5, 'john', 'some snippet', 0),
+(6, 'johndoe', 'some snippet', 0),
+(7, 'akselcc', 'some text here', 0),
+(8, 'user', 'My first snippet !', 0),
+(9, 'user', 'another public text for fun !', 1),
+(10, 'user', 'some text, will we see the redirection ??', 0);
 
 -- --------------------------------------------------------
 
@@ -52,7 +59,7 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` text NOT NULL,
   `password` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -60,7 +67,15 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `password`) VALUES
 (1, 'user123', 'somepw'),
-(2, 'userxyz', 'passwordscreeeet');
+(2, 'userxyz', 'passwordscreeeet'),
+(3, 'johndoe', 'password'),
+(4, 'zcababc', 'secretpassword'),
+(5, 'akselcakmak@gmail.com', 'sf'),
+(6, 'hj', 'vbnrth'),
+(7, 'akselc', 'pw'),
+(8, 'akselcc', 'password'),
+(9, 'user', 'pw'),
+(10, 'aks', 'secret');
 
 --
 -- Indexes for dumped tables
@@ -86,12 +101,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `snippets`
 --
 ALTER TABLE `snippets`
-  MODIFY `snippet_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `snippet_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
